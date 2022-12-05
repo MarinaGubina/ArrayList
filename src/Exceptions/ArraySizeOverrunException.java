6abd5246cdb447f20f622edddae368d7d4efeb55
gubina <gubinamarina1998@gmail.com>
2022-12-05 17:09:37 +0300

    ДЗ. Алгоритмы часть 1

---
+++ src/Exceptions/ArraySizeOverrunException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class ArraySizeOverrunException extends RuntimeException {
+
+    public ArraySizeOverrunException(String message) {
+        super(message);
+    }
+}
---
+++ src/Exceptions/ItemEqualsNullException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class ItemEqualsNullException extends RuntimeException{
+    public ItemEqualsNullException(String message) {
+        super(message);
+    }
+}
